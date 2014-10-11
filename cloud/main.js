e9835a2fa223b5bfccc4739b4e42683564df8b6a
lanstonpeng <lanstonpeng@gmail.com>
2014-10-11 12:54:12 +0800

    add token

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,7 @@
 require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
+AV.initialize("tkb80z5s7ezqq8et6fd26s6y6jz3u7eopgg5bboypom7ojk2", "s4784nd8ucpcp2083fgf2b246okzzy34agbghhf5zk2hee15");
 AV.Cloud.define("hello", function(request, response) {
   response.success("sup man");
 });
