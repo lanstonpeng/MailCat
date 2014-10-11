b9b2f7a8a0e7587512972c6ff33ed6f13b69f7f4
lanstonpeng <lanstonpeng@gmail.com>
2014-10-11 13:04:02 +0800

    ci

--- cloud/main.js
+++ cloud/main.js
@@ -2,9 +2,6 @@ require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 //
-var AV = require('avoscloud-sdk').AV;
-AV.initialize("tkb80z5s7ezqq8et6fd26s6y6jz3u7eopgg5bboypom7ojk2", "s4784nd8ucpcp2083fgf2b246okzzy34agbghhf5zk2hee15");
-
 AV.Cloud.define("hello", function(request, response) {
   response.success("sup man");
 });
@@ -13,7 +10,7 @@ AV.Cloud.define("averageStars", function(request, response) {
   var query = new AV.Query("TestObject");
   query.find({
     success: function(results) {
-      response.success(results);
+      response.success(results.length);
     },
     error: function() {
       response.error("movie lookup failed");
