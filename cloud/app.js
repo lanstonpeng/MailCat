784c15e3bd1edd6b5c8ea6bc777ecfdd93336e37
lanstonpeng <lanstonpeng@gmail.com>
2014-10-11 12:48:14 +0800

    test ci

--- cloud/app.js
+++ cloud/app.js
@@ -1,5 +1,6 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
+var mandrill = require('mandrill');
 var app = express();
 
 // App 全局配置
--- cloud/main.js
+++ cloud/main.js
@@ -2,5 +2,18 @@ require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("sup man");
 });
+
+AV.Cloud.define("averageStars", function(request, response) {
+  var query = new AV.Query("TestObject");
+  query.find({
+    success: function(results) {
+      response.success(results);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
+});
+
